c75640e90a155fb7c10e7a58595c154843021ac0
Lauro Pereira <lauropera01@gmail.com>
2022-08-24 03:14:28 -0300

    Adicionando link para CV e removendo projetos pouco desenvolvidos

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,8 +3,6 @@ import Project from '../components/Project';
 import dontTrumps from '../assets/projetos/dont-trumps-preview.png';
 import trybetunes from '../assets/projetos/trybetunes-preview.png';
 import shoppingCart from '../assets/projetos/shopping-cart-preview.png';
-import toDoList from '../assets/projetos/todo-list-preview.png';
-import trybewarts from '../assets/projetos/trybewarts-preview.png';
 
 import '../styles/Projects.css';
 
@@ -26,16 +24,6 @@ function Projects() {
           name="Shopping Cart"
           image={shoppingCart}
           link="https://lauropera.github.io/shopping-cart/"
-        />
-        <Project
-          name="To Do List"
-          image={toDoList}
-          link="https://lauropera.github.io/todo-list/"
-        />
-        <Project
-          name="Trybewarts"
-          image={trybewarts}
-          link="https://lauropera.github.io/trybewarts/"
         />
       </div>
     </section>
