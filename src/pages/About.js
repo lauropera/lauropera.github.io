8d97f06cf528ccf2ba2820a78e415a6b6f95c469
Lauro Pereira <lauropera01@gmail.com>
2022-08-28 23:38:20 -0300

    Atualizando idade

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,7 +11,7 @@ function About() {
       <div id="about" className="About-Me">
         <h2>Sobre mim</h2>
         <p>
-          Eu tenho 19 anos, nasci e moro na capital de São Paulo. Assim que
+          Eu tenho 20 anos, nasci e moro na capital de São Paulo. Assim que
           terminei o ensino médio eu estava entre ser professor ou biólogo
           marinho, mas quando conheci oque a programação pode fazer eu vi que
           consigo relacionar essas duas vontades! E quem está me ajudando a entrar
